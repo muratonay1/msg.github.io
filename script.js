c8f838904c9e99c54c23f040d7d455466bdba8eb
İsmet Murat ONAY <34923740+muratonay1@users.noreply.github.com>
2022-02-12 21:05:27 +0300

    profile Image fix

--- script.js
+++ script.js
@@ -36,7 +36,6 @@ var clicked_id = "";
                 firebase.database().ref('Messages/' + document.getElementsByClassName('contactUserProfileImage')[0].id + "/" + auth.username).push().set(context);
                 this.scrollToBottom();
                 this.$textarea.val('');
-                // responses
                 var templateResponse = Handlebars.compile($("#message-from").html());
                 var contextResponse = {
                     response: this.getRandomItem(this.messageResponses),
@@ -51,19 +50,13 @@ var clicked_id = "";
             }
         },
         addMessage: function() {
-            //içeriğini aldı
             this.messageToSend = this.$textarea.val();
-            //mesaj içeriğini aldıktan sonra render ediyor(ekrana basıyor)
             this.renderMessage();
         },
         addMessageEnter: function(event) {
-            // mesaj gönderme
             if (event.keyCode === 13) {
                 this.addMessage();
             }
-        },
-        contactClicked: function(params) {
-            //console.log(params);
         },
         scrollToBottom: function() {
             this.$chatHistory.scrollTop(this.$chatHistory[0].scrollHeight);
@@ -89,28 +82,21 @@ var clicked_id = "";
         },
         renderContactList: function() {
             var template = Handlebars.compile($("#contact-list").html());
-            //localde user bilgilerini koyduğumuz değişken
             var localUserList = {};
             firebase.database().ref("Users/").on("value", (users) => {
                 var items = users.val();
-                //refresh contact list
                 document.getElementById('list-contact').innerHTML = "";
                 Object.values(items).forEach((user) => {
                     if (user.username != window.localStorage.getItem('prettyAuth').jparse().jget('username')) {
                         localUserList.username = user.username;
                         localUserList.active = user.active ? "Online" : "Offline";
-                        //<li> iteminin id'sini userın token'ı yapıyoruz
                         localUserList.id = user.username + "/" + user.token;
                         localUserList.profileImage = user.profileImage;
                         if (user.active) {
-                            //online olan kaydı liste başına alma
                             this.$contactList.prepend(template(localUserList));
-                            //online kaydın border rengi
                             document.getElementById(localUserList.id).style.border = "3px solid lightgreen";
                         } else {
-                            //offline olaan kaydı liste sonuna ekleme
                             this.$contactList.append(template(localUserList));
-                            //offline olan kaydın border rengi
                             document.getElementById(localUserList.id).style.border = "3px solid red";
                         }
                         document.getElementById(localUserList.id).style.borderRadius = "50%";
@@ -173,10 +159,9 @@ function clickUser(params) {
             var auth = JSON.parse(window.localStorage.getItem('prettyAuth'));
             var clicked_username = clicked_id.split('/')[0];
             var clicked_activate = params.lastElementChild.children[1].textContent.trim();
-            var clicked_profile_image_id = clicked_id.split('/')[0];
             this.$chatHeaderUser[0].textContent = clicked_username;
             this.$contactUserProfileImage[0].id = clicked_username;
-            this.$contactUserProfileImage[0].src = clicked_profileImage;
+            this.$contactUserProfileImage[0].src = params.children[0].src;
             if (clicked_activate == "Online") {
                 document.getElementById(clicked_username).style.border = "3px solid lightgreen"
             } else if (clicked_activate == "Offline") {
@@ -231,10 +216,6 @@ function clickUser(params) {
     }
     contactListMessage.init();
 }
-$(document).ready(function() {
-    // Prepare the preview for profile picture
-
-});
 
 function readURL(input) {
     if (input.files && input.files[0]) {
@@ -242,17 +223,12 @@ function readURL(input) {
         var reader = new FileReader();
 
         reader.onload = function(e) {
-            // image resolution parameter increment = more hd && decrement bad resolution
             let image_resolution = 20;
-            //console.log(imageManipulation(e.target.result, 10, 1));
             imageManipulation(e.target.result, image_resolution, 1).then((res) => {
                 setTimeout(() => {
                     document.getElementById('wizardPicturePreview').src = res;
-                    //$("#wizardPicturePreview").attr("src", res).fadeIn("slow");
                     console.log("new image size => ", res.length, " KB");
                 }, 1000);
-
-
             });
         };
         reader.readAsDataURL(input.files[0]);
@@ -260,46 +236,35 @@ function readURL(input) {
 
     async function imageManipulation(dataUrl, targetFileSizeKb, maxDeviation = 50) {
         let originalFile = await urltoFile(dataUrl, "test.png", "image/png");
-        if (originalFile.size / 1000 < targetFileSizeKb) return dataUrl; // File is already smaller
-
+        if (originalFile.size / 1000 < targetFileSizeKb) return dataUrl;
         let low = 0.0;
         let middle = 0.5;
         let high = 1.0;
-
         let result = dataUrl;
-
         let file = originalFile;
-
         while (Math.abs(file.size / 1000 - targetFileSizeKb) > maxDeviation) {
             const canvas = document.createElement("canvas");
             const context = canvas.getContext("2d");
             const img = document.createElement("img");
-
             const promise = new Promise((resolve, reject) => {
                 img.onload = () => resolve();
                 img.onerror = reject;
             });
-
             img.src = dataUrl;
-
             await promise;
-
             canvas.width = Math.round(img.width * middle);
             canvas.height = Math.round(img.height * middle);
             context.scale(canvas.width / img.width, canvas.height / img.height);
             context.drawImage(img, 0, 0);
             file = await urltoFile(canvas.toDataURL(), "test.png", "image/png");
-
             if (file.size / 1000 < targetFileSizeKb - maxDeviation) {
                 low = middle;
             } else if (file.size / 1000 > targetFileSizeKb) {
                 high = middle;
             }
-
             middle = (low + high) / 2;
             result = canvas.toDataURL();
         }
-
         return result;
     }
 
@@ -327,8 +292,6 @@ function settingsOnChanges(visible) {
         document.getElementById('settingText-Username').value = storage.jget('username');
         document.getElementById('settingText-Password').value = storage.jget('password');
     }
-
-
 }
 
 function saveChanges() {
@@ -336,7 +299,5 @@ function saveChanges() {
     updated["profileImage"] = document.getElementById('wizardPicturePreview').src;
     firebase.database().ref("Users/" + updated.id).update(updated);
     window.localStorage.setItem('prettyAuth', updated.jstring());
-
-
 }
 document.getElementById('globalIp').innerText = localStorage.getItem('prettyAuth').jparse().jget('lastIp') + " Ip adresi üzerinden katılım sağlandınız.";
